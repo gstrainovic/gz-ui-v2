5e3d1fd0d6eeed59961425777526922c79554ce7
gstrainovic <g.strainovic@gmail.com>
2025-05-29 14:09:44 +0200

    myconfig

---
+++ src/composables/config.ts
@@ -0,0 +1,12 @@
+export const config = {
+  api: {
+    url: import.meta.env.VITE_API as string,
+    strapi: import.meta.env.VITE_STRAPI as string,
+  },
+};
+
+if (!config.api.url || !config.api.strapi) {
+  throw new Error('.env is missing VITE_API or VITE_STRAPI');
+}
+
+export default config;
