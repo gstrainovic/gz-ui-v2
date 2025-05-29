a5bbb68a53e735dee37935a169882a68e82de132
gstrainovic <g.strainovic@gmail.com>
2025-05-29 14:09:17 +0200

    pie colors

---
+++ src/composables/pieColors.ts
@@ -0,0 +1,14 @@
+export const pieColors = [
+  '#FF6384',
+  '#36A2EB',
+  '#FFCE56',
+  '#E7E9ED',
+  '#4BC0C0',
+  '#FF6384',
+  '#36A2EB',
+  '#FFCE56',
+  '#E7E9ED',
+  '#4BC0C0'
+];
+
+export default pieColors;
